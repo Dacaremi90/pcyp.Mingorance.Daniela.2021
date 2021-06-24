e3245efbdc2457dc31be2565f82c61cdefbc352c
Dacaremi90 <81193871+Dacaremi90@users.noreply.github.com>
2021-06-23 21:14:50 -0300

    WaitAllOneByOne
    Ejercicio 9

---
+++ PCyP.Ejercicios/Clase9.WaitAllOneByOne/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clase9.WaitAllOneByOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            //Lista de tareas
+            Task<int>[] tasks = new Task<int>[3];
+
+            //Ejecutar tarea
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+
+            //Mientras el largo de las tareas sea mayor a 0
+            while (tasks.Length > 0)
+            {
+               
+                int i = Task.WaitAny(tasks);   //Guarda la tarea que termina primero en i
+                
+                Task<int> completedTask = tasks[i]; //La tarea completada la guardo en un  variable task
+               
+                Console.WriteLine(sum += completedTask.Result);   //el resultado de la task lo sumo en sum
+                
+                var temp = tasks.ToList(); //hago un arrary temporal que copia atask
+               
+                temp.RemoveAt(i);   //remuevo la task del del indice i
+              
+                tasks = temp.ToArray();  //igualo task con la array temporal
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
